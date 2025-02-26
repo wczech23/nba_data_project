804fd57608965ba65b2a4c514dbe62479a426a56
wczech <wczech@umich.edu>
2025-02-26 16:31:05 -0500

    to csv

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -4,16 +4,16 @@ import openpyxl
 
 # converting coaching data files to csv
 
-#coaching_data_filepath = Path("coaching_data/")
-#for file in coaching_data_filepath.iterdir():
-    #if file.is_file():
-        #with open(file, "r") as file:
-            #excel_file = pd.read_html(file)
-            #filename = str(file).split("/")[1].split(".")[0]
-            #filepath = f"{str(coaching_data_filepath)}/{filename}"
-            #excel_file = excel_file[0] # modifying format of file to fit csv
-            #excel_file.columns = [' '.join(col).strip() for col in excel_file.columns.values]
-            #excel_file.to_csv(filepath, index=False)
+coaching_data_filepath = Path("coaching_data/")
+for file in coaching_data_filepath.iterdir():
+    if file.is_file():
+        with open(file, "r") as file:
+            excel_file = pd.read_html(file)
+            filename = str(file).split("/")[1].split(".")[0]
+            filepath = f"{str(coaching_data_filepath)}/{filename}"
+            excel_file = excel_file[0] # modifying format of file to fit csv
+            excel_file.columns = [' '.join(col).strip() for col in excel_file.columns.values]
+            excel_file.to_csv(filepath, index=False)
 
 
 # converting team data to csv
@@ -30,20 +30,20 @@ for file in team_data_filepath.iterdir():
 
 # converting player data to csv
 
-#sheet_names = range(2006,2026)
+sheet_names = range(2006,2026)
 
-# for sheet in sheet_names:
-    #sheet_data = pd.read_excel("player_data/nba_player_advstats.xlsx", sheet_name=str(sheet))
-    #sheet_data['Year'] = f"{sheet}"
-    #filepath = f"player_data/nba_player_advstats{sheet}"
-    #sheet_data.to_csv(filepath, index=False)
+for sheet in sheet_names:
+    sheet_data = pd.read_excel("player_data/nba_player_advstats.xlsx", sheet_name=str(sheet))
+    sheet_data['Year'] = f"{sheet}"
+    filepath = f"player_data/nba_player_advstats{sheet}"
+    sheet_data.to_csv(filepath, index=False)
 
 # converting player salaries to csv
-# for sheet in sheet_names:
-    #sheet_data = pd.read_excel("player_data/nba_player_salaries.xlsx", sheet_name=str(sheet))
-    #column_names = ['Player', 'Salary'] # modifying column data for uniform sql insert
-    #sheet_data = sheet_data.iloc[:, 1:3]
-    #sheet_data.columns = [f'{column_names[i]}' for i in range(len(sheet_data.columns))]
-    #sheet_data['Year'] = f"{sheet}"
-    #filepath = f"player_data/nba_player_salaries{sheet}"
-    #sheet_data.to_csv(filepath, index=False)
+for sheet in sheet_names:
+    sheet_data = pd.read_excel("player_data/nba_player_salaries.xlsx", sheet_name=str(sheet))
+    column_names = ['Player', 'Salary'] # modifying column data for uniform sql insert
+    sheet_data = sheet_data.iloc[:, 1:3]
+    sheet_data.columns = [f'{column_names[i]}' for i in range(len(sheet_data.columns))]
+    sheet_data['Year'] = f"{sheet}"
+    filepath = f"player_data/nba_player_salaries{sheet}"
+    sheet_data.to_csv(filepath, index=False)
--- rename_files.py
+++ rename_files.py
@@ -2,16 +2,16 @@ import re
 from pathlib import Path
 
 # getting file path for coaching_data
-folder_path = Path("team_data/")
+folder_path = Path("player_data/")
 
 
 for file in folder_path.iterdir():
     if file.is_file():
-        pattern = r'\((.*?)\)'
+        #print(file)
+        #pattern = r'\((.*?)\)'
         # Find all numbers
-        coaching_year = 2025 - int(re.findall(pattern, str(file))[0])
-        #filename = str(file).split(".")[0].split("/")[1]
-        
-        new_file = file.with_name(f"{coaching_year}_standings.xls")
+        #coaching_year = 2025 - int(re.findall(pattern, str(file))[0])
+        filename = str(file).split("/")[1] + ".csv"
+        new_file = file.with_name(filename)
         file.rename(new_file)
         
